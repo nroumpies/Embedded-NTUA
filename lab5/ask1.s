a6265dfbc7dc0167e1194fa2f53bc1e2e097e047
nroumpies <150586766+nroumpies@users.noreply.github.com>
2025-12-17 01:58:20 +0200

    Implement character reading and conversion in assembly

---
+++ lab5/ask1.s
@@ -0,0 +1,98 @@
+.section .data
+    buffer: .space 32
+    string: .asciz "Sequence of 32 chars:\n"
+
+.section .text
+.global main
+
+
+_start:main:
+       
+    MOV r0, #1          @ stdout
+    LDR r1, =string     @ buffer address
+    MOV r2, #22         @ number of bytes to write
+    MOV r7, #4          @ write: syscall
+    SVC 0
+
+    MOV r0, #0          @ stdin 
+    LDR r1, =buffer     @ buffer address
+    MOV r2, #32         @ number of bytes
+    MOV r7, #3          @ syscall: read
+    SVC 0
+
+    MOV r8, #0
+    MOV r4, r0          @ save number of bytes read
+    BL Read_and_Convert
+
+    MOV r0, #1          @ stdout
+    LDR r1, =buffer     @ buffer address
+    MOV r2, r4          @ number of bytes to write
+    MOV r7, #4          @ write: syscall
+    SVC 0
+
+    CMP r8, #27        @ Check if we need to end program
+    BNE main
+
+    MOV r0, #0
+    MOV r7, #1          @ syscall: exit
+    SVC 0
+
+Read_and_Convert:
+
+    MOV r5, r4         @ Counter for characters read
+    LDR r6, =buffer
+    CMP r4, #2 @ Check if one character was read
+    BEQ Check_Done
+
+loop:
+
+    LDRB r0, [r6], #1 @ Load first byte from buffer
+
+Check1:
+    CMP r0, #'a'        @ Compare with 'a'
+    BLT skip1
+    CMP r0, #'z'        @ Compare with 'z'
+    SUBLE r0, r0, #32
+    b skip3
+
+skip1:
+
+    CMP r0, #'A'        @ Compare with 'A'
+    BLT skip2
+    CMP r0, #'Z'        @ Compare with 'Z'
+    ADDLE r0, r0, #32   @ Convert to lowercase 
+    b skip3
+
+skip2:
+
+    CMP r0, #'0'        @ Compare with '0'
+    BLT skip3
+    CMP r0, #'9'        @ Compare with '9'
+    BGT skip3
+    ADD r0, r0, #5 
+    CMP r0, #'9'        @ Do +5 and if more than '9' for the - 10
+    SUBGT r0, r0, #10
+
+skip3:
+
+    STRB r0, [r6, #-1]   @ store back at the same address
+    SUB r5, r5, #1      @ Decrease counter
+    CMP r5, #0
+    BNE  loop
+    b end
+
+Check_Done:
+    LDRB r0, [r6]      @ Load first byte from buffer
+    MOV r8, #27        @ So that main ends program
+    CMP r0, #'q'
+    BEQ end 
+    CMP r0, #'Q'
+    BEQ end
+    MOV r8, #0
+    B Check1
+    
+
+end:
+
+    BX lr
+
